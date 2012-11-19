ef54506980ad3d396aab90fb3174cdf826c9cfb2
Beanu <beanu@ubuntu.ubuntu-domain>
2012-11-19 17:38:14 +0800

    add box2d to group

--- PhysicalWorld/src/com/zhaoyunhe/pw/props/Box2dAdapter.java
+++ PhysicalWorld/src/com/zhaoyunhe/pw/props/Box2dAdapter.java
@@ -1,32 +1,64 @@
 package com.zhaoyunhe.pw.props;
 
-import java.util.Iterator;
-
 import info.u250.c2d.engine.Engine;
 import info.u250.c2d.physical.box2d.Cb2ObjectGroup;
 import info.u250.c2d.physical.box2d.loader.cbt.CbtWorldReader;
 import info.u250.c2d.physical.box2d.loader.cbt.data.BodyData;
 import info.u250.c2d.physical.box2d.loader.cbt.data.JointData;
 
+import java.util.Iterator;
+
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.zhaoyunhe.pw.props.box2d.BoxHelper;
+import com.zhaoyunhe.pw.props.box2d.CircleHelper;
+import com.zhaoyunhe.pw.props.box2d.RotateHelper;
+import com.zhaoyunhe.pw.props.box2d.ScaleHelper;
+import com.zhaoyunhe.pw.props.box2d.SelectedHelper;
+import com.zhaoyunhe.pw.props.joints.DistanceJointHelper;
+import com.zhaoyunhe.pw.props.joints.FrictionJointHelper;
+import com.zhaoyunhe.pw.props.joints.PrismaticJointHelper;
 
 public class Box2dAdapter extends ShapeGroup {
-	final BoxHelper box;
+	final BoxHelper mBoxHelper;
+	final CircleHelper mCircleHelper;
+	final RotateHelper mRotateHelper;
+	final ScaleHelper mScaleHelper;
+	final SelectedHelper mSelectedHelper;
+	
+	final DistanceJointHelper mDistanceJointHelper;
+	final FrictionJointHelper mFrictionJointHelper;
+	final PrismaticJointHelper mPrismaticJointHelper; 
+	
 	public final CbtWorldReader data;
 	final Cb2ObjectGroup group;
 	final ShapeRenderer render;
 	boolean runMode = false;
 
 	public Box2dAdapter() {
-		box = new BoxHelper(this);
+		mBoxHelper = new BoxHelper(this);
+		mCircleHelper=new CircleHelper(this);
+		mRotateHelper=new RotateHelper(this);
+		mScaleHelper=new ScaleHelper(this);
+		mSelectedHelper=new SelectedHelper(this);
+		
+		mDistanceJointHelper=new DistanceJointHelper(this);
+		mFrictionJointHelper=new FrictionJointHelper(this);
+		mPrismaticJointHelper=new PrismaticJointHelper(this);
+		
 		data = new CbtWorldReader();
 		group = new Cb2ObjectGroup();
 		render = Engine.getShapeRenderer();
 		
 		//add to group
-		this.add(box);
+		this.add(mBoxHelper);
+		this.add(mCircleHelper);
+		this.add(mRotateHelper);
+		this.add(mScaleHelper);
+		this.add(mSelectedHelper);
+		this.add(mDistanceJointHelper);
+		this.add(mFrictionJointHelper);
+		this.add(mPrismaticJointHelper);
 	}
 
 	@Override
